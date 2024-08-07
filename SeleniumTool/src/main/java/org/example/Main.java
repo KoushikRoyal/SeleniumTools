e31bf2e974e5acd9657e28da4dd8661ab17d4bc1
koushik <akulakoushik2003@gmail.com>
2024-08-07 22:48:48 +0530

    opening browsers and testing the title

--- SeleniumTool/src/main/java/org/example/Main.java
+++ SeleniumTool/src/main/java/org/example/Main.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         WebDriver driver=new ChromeDriver();
-        driver.get("https://newerp.kluniversity.in/");
-        System.out.println(driver.getTitle());
+        driver.get("https://www.gmail.com");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("identifierId")).sendKeys("2100031449cseh@gmail.com");
+        Thread.sleep(2000);
+        if(driver.getTitle().equalsIgnoreCase("gmail")){
+            System.out.println("Test successful");
+        }
+        else{
+             System.out.println("Test failed");
+        }
+        driver.quit();
     }
 }
